ec663ccc6a36c0a45c0bfb409e1a1667bef94d4c
OneDeadFox <tjosephb.pro@gmail.com>
2023-01-26 21:49:58 -0800

    added event listener for search bar, attempted recurrsion

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,50 @@
         //c. what is it missing that is needed for app?
             //i. we will need to find another api for tournament data.
     //3. TODO: look into other needed apis to filling in gaps where needed
+//TODO: set up gatherer like query bars where each potential query item is in its own search bar
+    //1. maybe use a for loop to make the search bars based on an array of potential query items.
+    //2. have each search bar save the query with its associated search param into a variable that will be added to the fetch.
 
-const initialFetch = () => {
+
+//Global Variables========================================================
+//const fs = require('fs');
+
+const searchBar = $('.search-bar');
+
+let pageNum = 1;
+let results = [];
+
+//Event Listeners=========================================================
+searchBar.on('keydown', (e) => {
+    let key = e.which;
+    if(key === 13){
+        console.log('pressed enter')
+        initialFetch(searchBar.val());
+        searchBar.val('');
+    }
+})
+
+//Functions===============================================================
+const initialFetch = (query) => {
     const baseUrl = 'https://api.magicthegathering.io/';
     const version = 'v1/';
     //ards?setName=dissension&page=2 - this query will pull the second page of data from the set 'dissension'
-    const testQuery = 'cards?setName=dissension&page=2';
+    const testQuery = `cards?setName=${query}&page=${pageNum}`;
     const fetchUrl = baseUrl + version + testQuery;
 
     fetch (fetchUrl)
         .then((res) => {
             return res.json();
         })
-        .then((res) => {
-            console.log(res);
+        .then((data) => {
+            results.concat(data);
+            if(data.length === 100){
+                pageNum++;
+                initialFetch(query);
+            } else {
+                pageNum = 1;
+                console.log(results);
+            }
         })
 }
 
