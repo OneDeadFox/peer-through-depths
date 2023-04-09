bdca9c437306a2114f1078109f5e700e3a1c3bfa
OneDeadFox <tjosephb.pro@gmail.com>
2023-04-09 09:38:52 -0700

    added models index, and creaded db and dotenv

--- models/TagAssociation.js
+++ models/TagAssociation.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 class TagAssociation extends Model { }
 
 TagAssociation.init({
-    association: {
+    associationScore: {
         type: DataTypes.DECIMAL,
         allowNull: false,
     },
